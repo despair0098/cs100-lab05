51dbbe564051ebb9ecbe446357eac6c7305a5c34
despair0098 <zijunwang324@gmail.com>
2021-10-30 15:17:10 -0700

    Made Mult.cpp and CMakeLists.txt

---
+++ Mult.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+class Mult: public Base{
+	private:
+		double value;
+		Base* left;
+		Base* right;
+	public:
+		Mult(Base* a, Base* b){value = a.evaluate() * b.evaluate(); this->left = a; this->right = b;}
+		virtual double evaluate() {return value;}
+		virtual string stringify() {return "(" + left.stringify() + "*" +  right.stringify() + ")}"
+}
