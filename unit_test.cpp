f4716f8c0bc095c586e02da27e5dc872a643a997
YanaWanas <garrickchiu0723@gmail.com>
2021-10-31 16:10:36 -0700

    Added Rand test cases

--- unit_test.cpp
+++ unit_test.cpp
@@ -2,6 +2,11 @@
 
 #include "op_test.hpp"
 
+TEST(Rand1, RandNumber) {
+	Op* test = new Op(rand() % 100);
+	EXPECT_EQ(test->evaluate(), rand() % 100);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
