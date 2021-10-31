2fe911c10072a2f977920761c1b6ab0af2b8c9fb
YanaWanas <garrickchiu0723@gmail.com>
2021-10-31 15:09:45 -0700

    test

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,6 +1,22 @@
 #include "gtest/gtest.h"
 
 #include "op_test.hpp"
+/*
+TEST(Op1, Operand) {
+	Base* test1 = new PositiveOP();
+	Base* op = new Op(test1);
+	EXPECT_EQ(1, op->evaluate());
+	EXPECT_EQ("1.0", op->stringify());
+}
+
+
+TEST(Op2, OperandZero) {
+	Base* test2 = new ZeroOp();
+	Base* op = new Op(test2);
+	EXPECT_EQ(0, op->evaluate());
+	EXPECT_EQ("0.0", op->stringify());
+}
+*/
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
